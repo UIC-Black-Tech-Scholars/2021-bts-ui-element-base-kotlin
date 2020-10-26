03f80faf4151bdbbd4f57de2bd124f4c0f135b6c
lucasoskorep <lucas.oskorep@gmail.com>
2020-10-26 16:38:21 -0500

    Add in the updates for the stock project.

--- app/src/main/java/com/example/bts_ui_element_base/MainActivity.kt
+++ app/src/main/java/com/example/bts_ui_element_base/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.bts_ui_element_base
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var TAG:String = "MainActivity"
+    private lateinit var button:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.button)
+        button.setOnClickListener{ view ->
+            Toast.makeText(this, "You have pressed the button!!!", Toast.LENGTH_SHORT).show()
+            Log.d(TAG, "You have pressed the button, here is the log!!")
+        }
     }
 }
